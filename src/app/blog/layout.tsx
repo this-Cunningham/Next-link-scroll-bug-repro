0ae0c59c912975199d6deaadd9d2703fad2ed8a7
Chris_Cunningham <this.cunningham@gmail.com>
2023-03-24 00:17:05 -0400

    change height to 600px

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -3,8 +3,7 @@ import Link from 'next/link';
 export default function Layout ({ children }: { children: React.ReactNode }) {
   return (
     <>
-    {/* setting layout height to take up more than viewport to push children below the fold/initial viewport */}
-      <div style={{ height: '150vh' }}>
+      <div style={{ height: '600px' }}>
       <Link href='/' style={{ height: '100px', width: '100px', backgroundColor: 'green'}}>
         Click to go To Home
       </Link>
